2d0cb4c96d3cc02b488a76cb14194dff35226e82
Steven Baumann <steven.baumann@mymail.champlain.edu>
2020-08-24 22:32:08 -0400

    Added basic home page.

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import {Grid} from "@material-ui/core";
 
 const Home: React.FunctionComponent = () => (
-    <div>
-        {'test'}
-    </div>
+    <Grid
+        container
+        direction={'column'}
+        justify={'space-evenly'}
+        alignItems={'center'}
+    >
+        <h1>
+            {'Welcome to MoodWeb!'}
+        </h1>
+        <p>
+            {'Welcome to MoodWeb, the social network for sharing your moods.'}
+        </p>
+    </Grid>
 );
 
 export default Home;
